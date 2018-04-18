3dbe47a80694c741d28a7c4c63dae35f681b037a
Charise Walraven <cherri.walraven@gmail.com>
2018-04-18 11:14:56 +0200

    Add redux stuff, add components and connect to store. Add provider wrapper and ebay logo plus animation

---
+++ client/src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class ProductList extends PureComponent {
+  render() {
+    return (
+      <div>
+        <p>ProductList</p>
+      </div>
+    )
+  }
+}

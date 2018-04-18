fff140d795c7a9c3a039827ecc8618556eda6c8c
Charise Walraven <cherri.walraven@gmail.com>
2018-04-18 11:54:33 +0200

    Connect provider to store, modify intial state with dummy data, add mapStateToProps, loop through products from state in component and display on view

--- client/src/reducers/products.js
+++ client/src/reducers/products.js
@@ -1,3 +1,33 @@
-export default function(state = [], action = {}){
+const initialState = [
+  {
+    id: 1,
+    title: "Duck Liver Pate",
+    description: "It's yummy",
+    price: 4,
+    picture: "../media/duck.jpg",
+    email: "saucy@dsauce.com",
+    phone: "014 223 1567"
+  },
+  {
+    id: 2,
+    title: "Toothpaste",
+    description: "Minty fresh",
+    price: 2,
+    picture: "../media/toothpaste.jpg",
+    email: "brusher@fresh.com",
+    phone: "013 215 1679"
+  },
+  {
+    id: 3,
+    title: "Skort",
+    description: "Has a slight identity crisis",
+    price: 9,
+    picture: "../media/skort.jpg",
+    email: "confused@befuddled.com",
+    phone: "+27 4 773 5276"
+  }
+]
+
+export default function(state = initialState, action = {}){
   return state
 }
